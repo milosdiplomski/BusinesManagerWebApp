e8b07163981aff73f4ff3e2f84e5abcb430385b2
Milos <milos.mitrovicbgnp@gmail.com>
2020-09-16 23:24:10 +0200

    Create pages

---
+++ BusinesManagerWebApp/Pages/Provider/CreateProvider.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BusinesManagerWebApp.Pages.Provider
+{
+    public class CreateProviderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
